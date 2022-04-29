9eaab693c66742ba96bd734f702e3d499b8513c2
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-04-29 09:13:22 -0700

    memoized user action call

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,22 +7,22 @@ class UserHeader extends Component {
         this.props.fetchUser(this.props.userId);
     }
 
-  render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    render() {
+        const { user } = this.props;
 
-    if (!user) {
-        return null;
+        if (!user) {
+            return null;
+        }
+
+        return (
+            <div className='header'>{user.name}</div>
+        )
     }
-
-    return (
-      <div>UserHeader</div>
-    )
-  }
 }
 
-const mapStateToProps = (state) => {
-    return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(null, { fetchUser })(UserHeader);
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
 
